916d946b6b7a745309da410d2dc6e904bcf8f2e1
Jose Mendez <josealbertomendez13@gmail.com>
2025-10-18 00:24:07 -0500

    nuevo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,10 @@
 export default function Header() {
     return (
-        <header className="bg-gray-900 flex items-center p-3 w-full justify-between pb-20 pt-5">
+        <header className="bg-gray-900 flex items-center p-3 w-full justify-between pb-10 pt-5">
             <h1 className="text-white text-[15px] md:text-2xl font-bold tracking-tight">JM</h1>
             <nav>
                 <button className="text-white md:mr-50 flex justify-center px-3 py-3 items-center gap-8 bg-transparent font-bold rounded-lg shadow-md border-2 border-amber-100
-                    hover:scale-105 hover:shadow-lg transition-all duration-200 active:scale-95 focus-ring-2 text-[1.5rem] ">conversemos
+                    hover:scale-105 hover:shadow-lg transition-all duration-200 active:scale-95 focus-ring-2 text-[1rem] ">conversemos
 
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
                         <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 12.76c0 1.6 1.123 2.994 2.707 3.227 1.087.16 2.185.283 3.293.369V21l4.076-4.076a1.526 1.526 0 0 1 1.037-.443 48.282 48.282 0 0 0 5.68-.494c1.584-.233 2.707-1.626 2.707-3.228V6.741c0-1.602-1.123-2.995-2.707-3.228A48.394 48.394 0 0 0 12 3c-2.392 0-4.744.175-7.043.513C3.373 3.746 2.25 5.14 2.25 6.741v6.018Z" />
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,19 +3,19 @@ export default function Hero() {
         <section className="w-full bg-gray-900  mx-auto flex flex-col  justify-between pb-20">
             <div className="relative min-h-[300px] md:min-h-[550px] items-center">
 
-                <span className="text-white absolute left-0 md:left-0 font-bold text-[1.5rem] md:text-[3rem]">Hola soy</span>
+                <span className="text-white absolute left-0 md:left-0 md:top-7 font-bold text-[1.5rem] md:text-[2rem]">Hola soy</span>
 
                 <div className="flex flex-col pt-5 ">
-                    <span className="text-amber-50 text-[5rem] md:text-[25rem] leading-none text-left font-bold">Jose</span>
-                    <span className="text-amber-50 text-[5rem] md:text-[25rem] leading-none font-bold text-left">Mendez</span>
+                    <span className="text-amber-50 text-[5rem] md:text-[15rem] leading-none text-left font-bold md:pt-8">Jose</span>
+                    <span className="text-amber-50 text-[5rem] md:text-[15rem] leading-none font-bold text-left">Mendez</span>
                 </div>
 
-                <span className=" text-white absolute font-bold text-[1.5rem] md:text-[3rem] text-right md:top-200 right-50 md:right-380 d:text-[2rem] md:w-full md:text-right pb-10">Ingeniero de sistemas</span>
+                <span className=" text-white absolute font-bold text-[1.5rem] md:text-[2rem] text-right md:top-130 right-50 md:right-193 md:w-full md:text-right ">Ingeniero de sistemas</span>
 
-                <div className="pt-35 flex justify-between items-center w-full">
+                <div className="pt-20 flex justify-between items-center w-full">
 
-                    <button className="text-white flex justify-center px-5 py-2 items-center gap-8 md:ml-100 bg-transparent border-2 border-amber-100 font-bold rounded-lg shadow-md
-                    hover:scale-105 hover:shadow-lg transition-all duration-200 active:scale-95 focus-ring-2 text-[2rem]">Mis proyectos
+                    <button className="text-white flex justify-center px-5 py-2 items-center gap-3 md:ml-28 bg-transparent border-2 border-amber-100 font-bold rounded-lg shadow-md
+                    hover:scale-105 hover:shadow-lg transition-all duration-200 active:scale-95 focus-ring-2 text-[1rem]">Mis proyectos
 
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
                             <path stroke-linecap="round" stroke-linejoin="round" d="m8.25 4.5 7.5 7.5-7.5 7.5" />
@@ -23,8 +23,8 @@ export default function Hero() {
 
                     </button>
 
-                    <button className="text-white md:mr-100 flex justify-center px-5 py-2 items-center gap-8 bg-transparent font-bold rounded-lg shadow-md border-2 border-amber-100
-                    hover:scale-105 hover:shadow-lg transition-all duration-200 active:scale-95 focus-ring-2 text-[2rem] ">curriculum
+                    <button className="text-white md:mr-30 flex justify-center px-5 py-2 items-center gap-3 bg-transparent font-bold rounded-lg shadow-md border-2 border-amber-100
+                    hover:scale-105 hover:shadow-lg transition-all duration-200 active:scale-95 focus-ring-2 text-[1rem] ">curriculum
 
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
                             <path stroke-linecap="round" stroke-linejoin="round" d="m8.25 4.5 7.5 7.5-7.5 7.5" />
@@ -33,18 +33,68 @@ export default function Hero() {
                     </button>
 
                 </div>
+
             </div>
 
-            <div className="flex gap-5 items-center pt-30 justify-center">
+            <div className="flex gap-1 items-center pt-15 justify-center">
 
-                <a href = " https://www.youtube.com/watch?v=dQw4w9WgXcQ&list=RDdQw4w9WgXcQ&start_radio=1" className= "w-40 h-20 text-white  hover:scale-110 transition-all duration-200 " >
-                <svg className= "w-40 h-20" xmlns="http://www.w3.org/2000/svg" width="50" height="24" viewBox="0 0 50 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-instagram-icon lucide-instagram"><rect width="20" height="20" x="2" y="2" rx="5" ry="5"/><path d="M16 11.37A4 4 0 1 1 12.63 8 4 4 0 0 1 16 11.37z"/><line x1="17.5" x2="17.51" y1="6.5" y2="6.5"/></svg></a>
+                <a href=" https://www.youtube.com/watch?v=dQw4w9WgXcQ&list=RDdQw4w9WgXcQ&start_radio=1" className=" text-white  hover:scale-110 transition-all duration-200 ">
 
-                <a href = " https://www.youtube.com/watch?v=dQw4w9WgXcQ&list=RDdQw4w9WgXcQ&start_radio=1" className= "w-40 h-20 text-white  hover:scale-110 transition-all duration-200 items-center justify-center">
-                <svg className= "w-40 h-20" xmlns="http://www.w3.org/2000/svg" width="50" height="24" viewBox="0 0 50 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-github-icon lucide-github"><path d="M15 22v-4a4.8 4.8 0 0 0-1-3.5c3 0 6-2 6-5.5.08-1.25-.27-2.48-1-3.5.28-1.15.28-2.35 0-3.5 0 0-1 0-3 1.5-2.64-.5-5.36-.5-8 0C6 2 5 2 5 2c-.3 1.15-.3 2.35 0 3.5A5.403 5.403 0 0 0 4 9c0 3.5 3 5.5 6 5.5-.39.49-.68 1.05-.85 1.65-.17.6-.22 1.23-.15 1.85v4"/><path d="M9 18c-4.51 2-5-2-7-2"/></svg></a>
+                    <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        className="w-40 h-16"
+                        viewBox="0 0 24 24"
+                        fill="none"
+                        stroke="currentColor"
+                        strokeWidth="2"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                    >
+                        <rect width="20" height="20" x="2" y="2" rx="5" ry="5" />
+                        <path d="M16 11.37A4 4 0 1 1 12.63 8 4 4 0 0 1 16 11.37z" />
+                        <line x1="17.5" x2="17.51" y1="6.5" y2="6.5" />
+                    </svg>
 
-                <a href = " https://www.youtube.com/watch?v=dQw4w9WgXcQ&list=RDdQw4w9WgXcQ&start_radio=1" className= "w-40 h-20 text-white  hover:scale-110 transition-all duration-200 ">
-                <svg className= "w-40 h-20" xmlns="http://www.w3.org/2000/svg" width="50" height="24" viewBox="0 0 50 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-linkedin-icon lucide-linkedin"><path d="M16 8a6 6 0 0 1 6 6v7h-4v-7a2 2 0 0 0-2-2 2 2 0 0 0-2 2v7h-4v-7a6 6 0 0 1 6-6z"/><rect width="4" height="12" x="2" y="9"/><circle cx="4" cy="4" r="2"/></svg></a>
+
+
+                </a>
+
+
+                <a href=" https://www.youtube.com/watch?v=dQw4w9WgXcQ&list=RDdQw4w9WgXcQ&start_radio=1" className=" text-white  hover:scale-110 transition-all duration-200 items-center justify-center">
+                    <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        className="w-40 h-16"
+                        viewBox="0 0 24 24"
+                        fill="none"
+                        stroke="currentColor"
+                        strokeWidth="2"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                    >
+                        <path d="M15 22v-4a4.8 4.8 0 0 0-1-3.5c3 0 6-2 6-5.5.08-1.25-.27-2.48-1-3.5.28-1.15.28-2.35 0-3.5 0 0-1 0-3 1.5-2.64-.5-5.36-.5-8 0C6 2 5 2 5 2c-.3 1.15-.3 2.35 0 3.5A5.403 5.403 0 0 0 4 9c0 3.5 3 5.5 6 5.5-.39.49-.68 1.05-.85 1.65-.17.6-.22 1.23-.15 1.85v4" />
+                        <path d="M9 18c-4.51 2-5-2-7-2" />
+                    </svg>
+
+                </a>
+
+
+                <a href=" https://www.linkedin.com/in/jose-mendez-3b4b25210/" className=" text-white  hover:scale-110 transition-all duration-200 items-center justify-center">
+                    <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        className="w-40 h-16"
+                        viewBox="0 0 24 24"
+                        fill="none"
+                        stroke="currentColor"
+                        strokeWidth="2"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                    >
+                        <path d="M16 8a6 6 0 0 1 6 6v7h-4v-7a2 2 0 0 0-2-2 2 2 0 0 0-2 2v7h-4v-7a6 6 0 0 1 6-6z" />
+                        <rect width="4" height="12" x="2" y="9" />
+                        <circle cx="4" cy="4" r="2" />
+                    </svg>
+
+                </a>
 
             </div>
 
