1b66036f462d5e2acc72cc14202072cca9302e4c
Jose Mendez <josealbertomendez13@gmail.com>
2026-01-11 18:14:38 -0500

    modificando texto

--- src/data/projectsData.js
+++ src/data/projectsData.js
@@ -2,7 +2,7 @@ export const projectsData = [
     {
         id: 1,
         title: "ERP ligero",
-        shortDescription: "Solución ligera para ventas, inventario, facturación, clientes,usuario/roles, reportes y métricas en PyMEs.",
+        shortDescription: "Solución ligera para ventas, inventario, facturación, clientes, usuario/roles, reportes y métricas en PyMEs.",
         fullDescription: "Sistema de gestión modular diseñado para optimizar el flujo de ventas end-to-end, control de inventario y usuarios en tiempo real, análisis estratégico del negocio mediante métricas clave y seguridad basada en roles con Spring Security + JWT para control de acceso.  Desarrollo de backend con Java 17 y Spring Boot 3 siguiendo principios REST, validación de datos y separación por capas (controller/service/repository/mappers/dtos/entitys/exeptions/services/utils). Implementación de persistencia con Spring Data JPA y PostgreSQL para la gestión de datos de la empresa. Automatización de mapeo de DTOs/entidades con MapStruct + Lombok para reducir boilerplate y mantener consistencia del modelo. Despliegue y operación en entornos cloud utilizando Vercel, Railway y Cloudinary para almacenamiento y distribución de recursos. ",
         image: "https://res.cloudinary.com/dxl97cptv/image/upload/v1768147534/2e3d2346-786e-4136-a340-89cd0fe304fb.png",
         images: [
@@ -18,8 +18,8 @@ export const projectsData = [
     {
         id: 2,
         title: "Sistema de Gestión Integral para Oficina Médica",
-        shortDescription: "sistema de gestión para consultorios médicos",
-        fullDescription: "sistema de gestión para consultorios médicos desarrollado en Java con Spring Boot, orientado a administrar pacientes, citas, personal y procesos básicos de un consultorio u oficina médica moderna. Diseño de una arquitectura modular con seguridad basada en roles mediante Spring Security y JWT para el control de acceso, desarrollando el backend en Java 17 y Spring Boot 3 siguiendo principios REST, con persistencia implementada mediante Spring Data JPA y PostgreSQL para gestionar datos clínicos y administrativos de la oficina médica, automatización del mapeo entre DTOs y entidades usando MapStruct optimizada para mantener una serialización eficiente de los modelos, y una estrategia de pruebas y calidad soportada en Spring Test, testcontainers (PostgreSQL) y Mockito para validar la lógica y los componentes del sistema.",
+        shortDescription: "Sistema de gestión para consultorios médicos",
+        fullDescription: "Sistema de gestión para consultorios médicos desarrollado en Java con Spring Boot, orientado a administrar pacientes, citas, personal y procesos básicos de un consultorio u oficina médica moderna. Diseño de una arquitectura modular con seguridad basada en roles mediante Spring Security y JWT para el control de acceso, desarrollando el backend en Java 17 y Spring Boot 3 siguiendo principios REST, con persistencia implementada mediante Spring Data JPA y PostgreSQL para gestionar datos clínicos y administrativos de la oficina médica, automatización del mapeo entre DTOs y entidades usando MapStruct optimizada para mantener una serialización eficiente de los modelos, y una estrategia de pruebas y calidad soportada en Spring Test, testcontainers (PostgreSQL) y Mockito para validar la lógica y los componentes del sistema.",
         image: "https://res.cloudinary.com/dxl97cptv/image/upload/v1768170859/d30e0e75-b0fc-48ea-8bdd-d7b2986b8c6a.png",
         images: [
             "https://res.cloudinary.com/dxl97cptv/image/upload/v1768170859/d30e0e75-b0fc-48ea-8bdd-d7b2986b8c6a.png",
